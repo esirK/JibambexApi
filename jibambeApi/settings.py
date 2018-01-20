47b07917a029aae49bdd6eaeb155e98396f00435
esirK <esirkings@gmail.com>
2018-01-20 07:52:27 +0300

    Restructure files to fix import confilicts

--- jibambeApi/settings.py
+++ jibambeApi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'movies.apps.MoviesConfig'
 ]
 
 MIDDLEWARE = [
---
+++ movies/models.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+
+from django.db import models
+
+# Here we will have model for categories of movies and a Movie Item
+
+
+class MoviesCategories(models.Model):
+    name = models.CharField(max_length=120, null=False)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=120, null=False)
+    thumbnail = models.CharField(max_length=120, null=False)
+    source_url = models.CharField(max_length=500, null=False)
+    duration = models.TimeField(default=timedelta(hours=2))
+    added_on = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(MoviesCategories, related_name="movies", on_delete=models.DO_NOTHING)
+
+    class Meta:
+        ordering = ('added_on',)
