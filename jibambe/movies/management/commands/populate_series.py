32db8ef9d3797b234b518aa5d07b0c9efa31b052
esirK <esirkings@gmail.com>
2018-02-07 05:37:34 +0300

    [Feature] Create a populate series command that populates different series into the database from the file system.

---
+++ jibambe/movies/management/commands/populate_series.py
@@ -0,0 +1,52 @@
+import urllib
+
+from django.core.management import BaseCommand
+from termcolor import colored
+
+from jibambe.movies.serializers import SerieSerializer
+
+
+class Command(BaseCommand):
+    help = "Populates Database with Different series"
+
+    def handle(self, *args, **options):
+        import os
+        os.chdir('jibambe/movies/static/series')
+        # List all directories in the series folder
+        series = os.listdir('.')
+
+        url = "http://192.168.1.143:8080/static/series/"
+        thumbnail = ""
+
+        for single_series in series:
+            # Pick the series name and inside its folder, pick its thumbnail
+            os.chdir(single_series)
+            seasons = os.listdir('.')  # All seasons are contained inside a single series folder
+
+            num_seasons = len(seasons)
+
+            for season in seasons:
+                if season.endswith(".jpg") | season.endswith(".png") | season.endswith('.jpeg'):
+                    num_seasons -= 1
+                    # Its the series thumbnail
+                    print("Found a thumbnail... ", season)
+                    thumbnail = url + single_series + '/' + season
+                    thumbnail = urllib.parse.quote(thumbnail, safe=":,/")
+
+            series_found = SerieSerializer(data={'name': single_series, 'thumbnail': thumbnail,
+                                                 'seasons': num_seasons})
+
+            print(colored("Trying to add `" + single_series + "` to database", "yellow"))
+
+            if series_found.is_valid():
+                try:
+                    series_found.save()
+                    print(colored('Successfully Populated database with {0}  Series'
+                                  , "green").format(single_series))
+                except Exception as e:
+                    print(colored("Error ", "red"), e)
+            else:
+                self.stderr.write(self.style.ERROR("Series '{0}' not valid: {1}".
+                                                   format(single_series, series_found.errors)))
+            # navigate back one dir up
+            os.chdir('..')
