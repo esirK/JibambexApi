c7831b28900be02f1df70d96f2cf349b8d1f608e
esirK <esirkings@gmail.com>
2018-02-07 05:35:48 +0300

    [Chore] Create serializer for series and season models for use in creating new objects and returning serialized objects in requests

--- jibambe/movies/serializers.py
+++ jibambe/movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from jibambe.movies.models import MoviesCategories, Movie
+from jibambe.movies.models import MoviesCategories, Movie, Series, Season
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -21,3 +21,22 @@ class SingleCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MoviesCategories
         fields = ('movies',)
+
+
+class SingleSeasonSerializer(serializers.ModelSerializer):
+    """
+    Serializer for single Season
+    """
+    class Meta:
+        model = Season
+        fields = ('name', 'thumbnail', 'num_episodes', 'series', 'id')
+
+
+class SerieSerializer(serializers.ModelSerializer):
+    seasons = SingleSeasonSerializer(many=True, read_only=True)
+    """Serializer for the Series model"""
+    class Meta:
+        model = Series
+        fields = ('name', 'thumbnail', 'seasons', 'id')
+
+
