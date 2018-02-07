a2d603f81b3caaf0612c16e2cb1099c0d48d7738
esirK <esirkings@gmail.com>
2018-02-07 05:41:18 +0300

    Add Series and Season models into the admin site.

--- jibambe/movies/admin.py
+++ jibambe/movies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import MoviesCategories, Movie
+from .models import MoviesCategories, Movie, Series, Season
 
 admin.site.register(MoviesCategories)
 admin.site.register(Movie)
+admin.site.register(Series)
+admin.site.register(Season)
