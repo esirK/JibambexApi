e18f283f57172ab92bcbe1fe040fd49a202f620c
esirK <esirkings@gmail.com>
2018-02-07 05:34:03 +0300

    [Chore] Create a Series and a Season models for a single sel=ries and season respectively.

--- jibambe/movies/models.py
+++ jibambe/movies/models.py
@@ -17,7 +17,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=1000, null=False, unique=True)
     thumbnail = models.TextField(max_length=1200, null=False)
     source_url = models.CharField(max_length=5000, null=False)
-    duration = models.TimeField(default=timedelta(hours=2))
+    duration = models.CharField(max_length=100, null=False)
     added_on = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(MoviesCategories, related_name="movies", on_delete=models.DO_NOTHING)
 
@@ -26,3 +26,28 @@ class Movie(models.Model):
 
     class Meta:
         ordering = ('added_on',)
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=1000, null=False, unique=True)
+    thumbnail = models.TextField(max_length=1200, null=False)
+    seasons_num = models.CharField(max_length=100, null=False, unique=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Series"
+
+
+class Season(models.Model):
+    """
+    Model for a single season in a series
+    """
+    name = models.CharField(max_length=1000, null=False, unique=True)
+    thumbnail = models.TextField(max_length=1200, null=False)
+    num_episodes = models.CharField(max_length=100, null=False, unique=False)
+    series = models.ForeignKey(Series, related_name="seasons", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
